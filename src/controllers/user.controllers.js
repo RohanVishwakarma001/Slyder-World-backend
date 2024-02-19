02c558b9367a41d76633755c6f062490381bf2e3
Rohan Vishwakarma <rohanvishwakarma8261@gmail.com>
2024-02-19 12:21:42 +0530

    create regiser controller and bug fix

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -1,9 +1,63 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { apiError } from "../utils/apiError.js";
+import { User } from "../models/user.models.js";
+import { uploadOnCloudinery } from "../utils/fileUplode.js";
+import { apiResponce } from "../utils/apiResponce.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({
-    name: "hello",
+  const { fullName, email, username, password } = req.body;
+  console.log(`Email:`, email);
+
+  // if (fullName === "") {
+  //   throw new apiError(400, "Full Name is required");
+  // }
+  if (
+    [fullName, email, username, password].some(
+      (fields) => fields?.trim() === ""
+    )
+  ) {
+    throw new apiError(400, "Full Name is required");
+  }
+
+  const existedUser = User.findOne({
+    $or: [{ username }, { email }],
   });
+  if (existedUser) {
+    throw new apiError(409, "User Already exist");
+  }
+
+  const avatarLocalPath = req.fields?.avatar[0]?.path;
+  const coverImageLocalPath = req.fields?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw apiError(400, "Avatar is required");
+  }
+  const avatar = await uploadOnCloudinery(avatarLocalPath);
+  const coverImage = await uploadOnCloudinery(coverImageLocalPath);
+
+  if (!avatar) {
+    throw apiError(400, "Avatar is required");
+  }
+  const user = await User.create({
+    fullName,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+    email,
+    password,
+    username: username.lovercase,
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new apiError(500, "Something went wrong white registration.");
+  }
+
+  return res
+    .status(200)
+    .json(new apiResponce(200, createdUser, "User registered successfully"));
 });
 
 export { registerUser };
